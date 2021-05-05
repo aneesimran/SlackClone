62f1677093c1fac0c234cb89604cc32fdafd937d
Anees Imran <aneesimran@outlook.com>
2021-05-05 12:17:34 +0100

    styled components

---
+++ slackclone/src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from "styled-components";
+
+function Header() {
+    return (
+      <HeaderContainer>
+        {/* Header left */}
+        <h1>test</h1>
+        {/* Header search */}
+
+        {/* Header right */}
+      </HeaderContainer>
+    );
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+    color: red;
+`;
---
+++ slackclone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCp1ZimRfjMxHnmPU140hI519jsqwYxVGk",
+  authDomain: "slackclone-79e7d.firebaseapp.com",
+  projectId: "slackclone-79e7d",
+  storageBucket: "slackclone-79e7d.appspot.com",
+  messagingSenderId: "259405550759",
+  appId: "1:259405550759:web:4fa0b7870b475b9411f782",
+};
